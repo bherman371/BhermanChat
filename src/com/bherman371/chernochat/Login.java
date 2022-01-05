2f4abdc8d277d89372deb487af7a33cf318930bb
Benjamin Herman <bherman371@gmail.com>
2022-01-05 16:44:10 -0500

    Created the login window

--- src/com/bherman371/chernochat/Login.java
+++ src/com/bherman371/chernochat/Login.java
@@ -1,24 +1,42 @@
 package com.bherman371.chernochat;
 
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
 
 public class Login extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+	private JTextField txtName;
 
 	public Login() {
+		setResizable(false);
+		setTitle("Login");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setSize(300, 380);
+		setLocationRelativeTo(null);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		txtName = new JTextField();
+		txtName.setText("name");
+		txtName.setBounds(69, 62, 148, 24);
+		contentPane.add(txtName);
+		txtName.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("Name:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel.setBounds(125, 39, 35, 13);
+		contentPane.add(lblNewLabel);
 	}
 	
 	public static void main(String[] args) {
@@ -33,5 +51,4 @@ public class Login extends JFrame {
 			}
 		});
 	}
-
 }

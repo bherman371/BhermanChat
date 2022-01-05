0ab2a90351066f0c52e0895566d31cd2b01c0f83
Benjamin Herman <bherman371@gmail.com>
2022-01-05 17:07:25 -0500

    Created the Login GUI

--- src/com/bherman371/chernochat/Login.java
+++ src/com/bherman371/chernochat/Login.java
@@ -8,15 +8,31 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import java.awt.Font;
+import javax.swing.JButton;
 
 public class Login extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField txtName;
+	private JTextField txtAddress;
+	private JLabel lblAddress;
+	private JLabel lblPort;
+	private JTextField txtPort;
+	private JLabel lblAddressDesc;
+	private JLabel lblPortDesc;
 
 	public Login() {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 		setResizable(false);
 		setTitle("Login");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -32,10 +48,45 @@ public class Login extends JFrame {
 		contentPane.add(txtName);
 		txtName.setColumns(10);
 		
-		JLabel lblNewLabel = new JLabel("Name:");
-		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
-		lblNewLabel.setBounds(125, 39, 35, 13);
-		contentPane.add(lblNewLabel);
+		JLabel lblName = new JLabel("Name:");
+		lblName.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblName.setBounds(125, 39, 35, 13);
+		contentPane.add(lblName);
+		
+		txtAddress = new JTextField();
+		txtAddress.setColumns(10);
+		txtAddress.setBounds(69, 131, 148, 24);
+		contentPane.add(txtAddress);
+		
+		lblAddress = new JLabel("IP Address:");
+		lblAddress.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblAddress.setBounds(112, 108, 65, 13);
+		contentPane.add(lblAddress);
+		
+		lblPort = new JLabel("Port:");
+		lblPort.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblPort.setBounds(130, 189, 29, 13);
+		contentPane.add(lblPort);
+		
+		txtPort = new JTextField();
+		txtPort.setColumns(10);
+		txtPort.setBounds(69, 212, 148, 24);
+		contentPane.add(txtPort);
+		
+		lblAddressDesc = new JLabel("(eg. 192.168.0.2)");
+		lblAddressDesc.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblAddressDesc.setBounds(92, 160, 101, 13);
+		contentPane.add(lblAddressDesc);
+		
+		lblPortDesc = new JLabel("(eg. 8192)");
+		lblPortDesc.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblPortDesc.setBounds(112, 240, 61, 13);
+		contentPane.add(lblPortDesc);
+		
+		JButton btnLogin = new JButton("Login");
+		btnLogin.setFont(new Font("Tahoma", Font.PLAIN, 14));
+		btnLogin.setBounds(100, 281, 85, 24);
+		contentPane.add(btnLogin);
 	}
 	
 	public static void main(String[] args) {
